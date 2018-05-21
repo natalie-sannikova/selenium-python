b2f4ee1283d1cc352423611f12de91d6c1c6bb66
Natalia Sannikova <sanny.natalie@gmail.com>
2018-05-20 21:07:26 -0400

    switch to pytest

---
+++ tests/Coursera.py
@@ -0,0 +1,6 @@
+score = input("Enter Score: ")
+scr = float (score)
+if scr < 0.0 or scr > 1.0:
+    print ("Error. Enter valid score")
+    quit()
+
---
+++ tests/test_selenium.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class TestClass(object):
+
+    def test_chrome(self, selenium: WebDriver):
+        selenium.implicitly_wait(10)
+        selenium.get("https://google.com")
+        queryInput = selenium.find_element_by_name("q")
+        queryInput.send_keys("hello world")
+        queryInput.submit()
+        resultStats = selenium.find_element_by_id("resultStats")
+        if "About" not in resultStats.text:
+            assert False
+
+    def test_chrome2(self, selenium: WebDriver):
+        firstResult = selenium.find_element_by_xpath("//*[@id=\"rso\"]/div/div/div[1]/div/div/h3/a")
+        firstResult.click()
+        if "Wikipedia" not in selenium.title:
+            assert False
+
