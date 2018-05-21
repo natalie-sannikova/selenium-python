5a16240a64a1fce5e5c42ed88f8c6ada17f0ea33
Natalia Sannikova <sanny.natalie@gmail.com>
2018-05-20 21:19:56 -0400

    fix tests

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -7,13 +7,18 @@ class TestClass(object):
         selenium.implicitly_wait(10)
         selenium.get("https://google.com")
         queryInput = selenium.find_element_by_name("q")
-        queryInput.send_keys("hello world")
+        queryInput.send_keys("hello world wiki")
         queryInput.submit()
         resultStats = selenium.find_element_by_id("resultStats")
         if "About" not in resultStats.text:
             assert False
 
     def test_chrome2(self, selenium: WebDriver):
+        selenium.implicitly_wait(10)
+        selenium.get("https://google.com")
+        queryInput = selenium.find_element_by_name("q")
+        queryInput.send_keys("hello world wiki")
+        queryInput.submit()
         firstResult = selenium.find_element_by_xpath("//*[@id=\"rso\"]/div/div/div[1]/div/div/h3/a")
         firstResult.click()
         if "Wikipedia" not in selenium.title:
